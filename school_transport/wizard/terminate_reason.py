daea7ead7fe395a4504d6515173e2624edda2df5
RiteshSerpentCS <ritesh.b.serpentcs@gmail.com>
2024-04-01 15:29:47 +0530

    [MIG] School Transport: Migrated school transport into 16.0 and solved pre-commits

--- school_transport/wizard/terminate_reason.py
+++ school_transport/wizard/terminate_reason.py
@@ -7,16 +7,12 @@ from odoo import api, fields, models
 class TerminateReasonTransport(models.TransientModel):
     _inherit = "terminate.reason"
 
-    transport_info = fields.Text(
-        "Transport Info", help="Enter transport information"
-    )
+    transport_info = fields.Text(help="Enter transport information")
 
     @api.model
     def default_get(self, fields):
         res = super(TerminateReasonTransport, self).default_get(fields)
-        student_rec = self.env["student.student"].browse(
-            self._context.get("active_id")
-        )
+        student_rec = self.env["student.student"].browse(self._context.get("active_id"))
         student_transport_rec = self.env["transport.registration"].search(
             [
                 ("student_id", "=", student_rec.id),
--- school_transport/wizard/transfer_vehicle.py
+++ school_transport/wizard/transfer_vehicle.py
@@ -13,12 +13,8 @@ class TransferVehicle(models.TransientModel):
         "transport.participant", "Participation", required=True
     )
     root_id = fields.Many2one("student.transport", "Root", required=True)
-    old_vehicle_id = fields.Many2one(
-        "fleet.vehicle", "Old Vehicle No", required=True
-    )
-    new_vehicle_id = fields.Many2one(
-        "fleet.vehicle", "New Vehicle No", required=True
-    )
+    old_vehicle_id = fields.Many2one("fleet.vehicle", "Old Vehicle No", required=True)
+    new_vehicle_id = fields.Many2one("fleet.vehicle", "New Vehicle No", required=True)
 
     @api.model
     def default_get(self, fields):
@@ -52,22 +48,14 @@ class TransferVehicle(models.TransientModel):
             # First Check Is there vacancy or not
             person = int(vehi_data.participant) + 1
             if vehi_data.capacity < person:
-                raise UserError(
-                    _("Error ! There is No More vacancy on this vehicle.")
-                )
+                raise UserError(_("Error ! There is No More vacancy on this vehicle."))
             # remove entry of participant in old vehicle.
-            participants = [
-                prt_id.id for prt_id in vehi_data.vehi_participants_ids
-            ]
+            participants = [prt_id.id for prt_id in vehi_data.vehi_participants_ids]
             if rec.participation_id.id in participants:
                 participants.remove(rec.participation_id.id)
             vehi_data.write({"vehi_participants_ids": [(6, 0, participants)]})
             # entry of participant in new vehicle.
-            participants = [
-                prt_id.id for prt_id in vehi_new_data.vehi_participants_ids
-            ]
+            participants = [prt_id.id for prt_id in vehi_new_data.vehi_participants_ids]
             participants.append(rec.participation_id.id)
-            vehi_new_data.write(
-                {"vehi_participants_ids": [(6, 0, participants)]}
-            )
+            vehi_new_data.write({"vehi_participants_ids": [(6, 0, participants)]})
             rec.participation_id.write({"vehicle_id": rec.new_vehicle_id.id})
